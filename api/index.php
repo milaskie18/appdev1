02c398b8ddc0dee4f053e0306dde345417c3aca8
Jamila Omalan <jamilaomalan13@gmail.com>
2026-02-02 12:01:14 +0800

    second commit

---
+++ api/index.php
@@ -0,0 +1,32 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Appdev Activity</title>
+    </head>
+    <body>
+        <h1>
+            Welcome to my Application Development Activity
+        </h1>
+
+        <div>
+        <p>
+            Where you can find my name and profile under this section.
+        </p>
+        </div>
+
+        <h2>
+            Name: Jamila B. Omalan 
+        </h2>
+
+        <br>
+        <p id="real">
+            Here's my picture
+        </p>
+
+        <img src="assets/favorite.jpg" id="favorite" alt="Favorite word 'favorite' in a pink cloud">
+
+    </body>
+    </html>
